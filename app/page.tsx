e7b17020fcf41695d5872dadd550b0bb2a2134ee
keith <keithneo00@gmail.com>
2024-12-29 18:18:19 +0800

    refactor(page.tsx): Added styles for about me page Added style for image and text for the page. Will need to add styling for text overlap on images.
    feat(main_about_me.png): Added new about me image

--- app/page.tsx
+++ app/page.tsx
@@ -9,34 +9,38 @@ export default function Home() {
       </header>
       <section className="main flex mt-20">
         <section className="left md:w-1/2 flex items-center justify-center relative">
+          <img 
+          src="images/main_about_me.png" 
+          alt="about_me_1" 
+          className="w-[28rem] h-[34rem]"
+          />
         {/* will redo the image soon */}
         
           {/* outer circle shape  */}
-          <div className="relative w-[28rem] h-[28rem] overflow-hidden rounded-full bg-gray-700">
+          {/* <div className="relative w-[28rem] h-[28rem] overflow-hidden rounded-full bg-gray-700"> */}
             {/* image inside the shape */}
-            <div className="absolute w-[28rem] h-[28rem] flex items-center justify-center">
-              <img 
-                src="images/about_me_1.png" 
-                alt="about_me_1" 
-                className="w-[28rem] h-[28rem] object-cover"
-              />
-            </div>
-          </div>
+            {/* <div className="absolute w-[28rem] h-[28rem] flex items-center justify-center"> */}
+              {/* <img  */}
+                {/* // src="images/about_me_1.png"  */}
+                {/* // alt="about_me_1"  */}
+                {/* // className="w-[28rem] h-[28rem] object-cover" */}
+              {/* // /> */}
+            {/* </div> */}
+          {/* </div> */}
         </section>
-        <section className="right md:w-1/2">
-          <div className="text text-white text-left m-auto">
-            <span>Hello! I am</span> <br />
-            <span>Keith.</span> <br />
-            <span>I am an Ai and Cybersecurity specialist</span>
+        <section className="right md:w-1/2 flex flex-col justify-center">
+          <div className="text text-white text-left">
+            <span className="text-4xl">Hello! I am</span> <br />
+            <span className="text-7xl text-red-400">Keith.</span> <br />
+            <span className="text-4xl">I am an Ai and Cybersecurity specialist</span>
             <br /> <br />
-            <p>
+            <p className="text-md">
               Welcome to my portfolio! Here, I showcase a range of projects spanning 
-              <span>Kubernetes orchestration, frontend and backend development, software tools,</span> 
-              and <span>CI/CD pipelines.</span> 
-              While my expertise lies in <span>
-              Artificial Intellegience
-              </span> and 
-              <span>Cybersecurity</span>
+              <span className="text-rose-400"> Kubernetes orchestration, frontend and backend development, software tools,</span> 
+              and <span className="text-rose-400">CI/CD pipelines. </span> 
+              While my expertise lies in 
+              <span className="text-rose-400"> Artificial Intellegience</span> and 
+              <span className="text-rose-400"> Cybersecurity</span>
               , I thrive on building scalable solutions that bridge these fields with modern software technologies.
             </p>
           </div>
