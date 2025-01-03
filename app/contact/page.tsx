a7351b4a0b560efef4d3bab9dec89433aa419e66
keith <keithneo00@gmail.com>
2025-01-04 00:31:27 +0800

    feat(api/contact/, components/contactForm.tsx): Created a working contact form The contact form is done outside the main page as a component to use client side rendering. The backend for the form uses an api called contact which takes the form data and parses it using `nodemailer` and sends an email using my `no reply` email to my main email.
    For this part user will have to create a `.env.local` file and have
    EMAIL_USER=<email_app_will_use>
    EMAIL_PASS=<email_app_pass>
    RECIPIENT_EMAIL=<user_personal_email>
    
    refactor(contact/page.tsx, package-lock.json, package.json):
    Created the main placeholders for the contact page. Also added
    `nodemailer` package to the project.

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,11 +1,24 @@
 // Contact page
 import NavBar from "../components/navbar";
+import Footer from "../components/footer";
+import ContactForm from "../components/contactForm";
 
 export default function Contact() {
     return (
         <main>
             <NavBar />
-            <h1>This is the contact page</h1>
+            {/* main sectino */}
+            <section className="w-full h-auto flex flex-row my-10">
+                {/* text section left */}
+                <section className="w-1/2 h-auto border border-purple-400">
+                    <h1 className="text-gray-200">Text here</h1>
+                </section>
+                {/* contact form right */}
+                <section className="w-1/2 h-auto flex">
+                    <ContactForm />
+                </section>
+            </section>               
+            <Footer />
         </main>
     );
 }
