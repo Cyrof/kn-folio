7361f880af990c96067c55de17f1d7969407fc9a
keith <keithneo00@gmail.com>
2025-04-28 01:41:52 +0800

    refactor(projects, skills, processed-data, myWork/page): Convert to mantine components Change the processed data json file to use the new path of the images
    The images will now be within the github repository itself instead of
    dynamically downloading it from google drive since it does not work too
    well.
    
    refactor(images/projects): Moved project images to its own folder
    Done this for organisation purposes

--- app/components/projects.tsx
+++ app/components/projects.tsx
@@ -3,6 +3,7 @@
 import { useEffect, useState } from "react"
 import { Data } from "../interfaces/skills_project"
 import { chunkArray } from "../utils/chunkArray"
+import { Box, Button, Center, Group, Image, Stack, Text } from "@mantine/core"
 
 const Projects = () => {
     const [data, setData] = useState<Data>({ skills: [], projects: [] });
@@ -25,39 +26,44 @@ const Projects = () => {
     const projectChunk = chunkArray(data.projects, 3);
 
     return(
-        <div className="w-full h-auto flex flex-col gap-10 items-center animate-staggered-text-down [--slidein-delay:200ms] opacity-0 [animation-timeline:view()]">
+        <Stack className="w-full h-auto animate-staggered-text-down [--slidein-delay:200ms] opacity-0 [animation-timeline:view()]" align="center" gap="lg">
             {projectChunk.map((chunk, chunkIndex) => (
-                <div key={chunkIndex} className="flex justify-center w-full gap-20">
-                    {chunk.map((proj, index) => (
-                        <div
+                <Group key={chunkIndex} className="w-full" justify="center" gap="xl">
+                    {chunk.map((project, index) => (
+                        <Box
                             key={index}
                             className="group w-56 h-56 m-0 relative transition-all duration-500 ease-in-out hover:h-[19rem] hover:w-[17rem]"
                         >
                             {/* wrapper */}
-                            <div
-                                className="bg-[#3d3131] m-0 h-full w-full rounded-xl shadow-xl flex flex-col justify-center items-center gap-3 relative"
-                            >
-                                <div className="w-full flex flex-col items-center gap-3 transition-transform duration-500 [transform-style:preserve-3d] group-hover:-translate-y-10">
+                            <Stack className="bg-[#3d3131] m-0 h-full w-full rounded-xl shadow-xl relative" justify="center" align="center" gap={3}>
+                                <Stack className="w-full transition-transform duration-500 [transform-style:preserve-3d] group-hover:-translate-y-10" gap={3} align="center">
                                     {/* square image */}
-                                    <div className="w-24 h-24 border-4 border-gray-900 shadow-lg bg-gray-700 flex justify-center items-center rounded-xl">
-                                        <img src={proj.url} alt="img" className="w-20 h-auto"/>
-                                    </div>
-                                    <span className="w-4/5 h-10 text-center font-sans font-bold text-lg">{proj.name}</span>
-                                </div>
-                                {/* <p className="absolute w-4/5 text-center font-sans font-medium bottom-0 opacity-0 group-hover:animate-staggered-text-up group-hover:[--slidein-delay:200ms]">{proj.description}</p> */}
-                                <div className="absolute mb-2 font-sans font-medium bottom-0 opacity-0 flex flex-col gap-3 items-center text-center group-hover:animate-staggered-text-up group-hover:[--slidein-delay:100ms]">
-                                    <p className="w-4/5">{proj.description}</p>
-                                    {/* github button */}
-                                    <a href={proj.github} target="_blank" className=" bg-red-500 rounded-xl text-gray-200 p-1 hover:bg-red-400 hover:text-gray-900 transition-colors duration-300">
+                                    <Center className="w-24 h-24 border-4 border-gray-900 shadow-lg bg-gray-700 rounded-xl">
+                                        <Image 
+                                            src={project.url}
+                                            alt="img"
+                                            className="w-20 h-auto"
+                                        />
+                                    </Center>
+                                    <Text className="w-4/5 h-10 text-center font-sans font-bold text-lg text-gray-200">{project.name}</Text>
+                                </Stack>
+                                <Stack className="absolute mb-2 font-sans font-medium bottom-0 opacity-0 text-center group-hover:animate-staggered-text-up group-hover:[--slidein-delay:100ms" align="center" gap={3}>
+                                    <Text className="w-4/5 text-gray-200">{project.description}</Text>
+                                    <Button
+                                        component="a"
+                                        href={project.github}
+                                        target="_blank"
+                                        className="bg-red-500 rounded-xl text-gray-200 p-1 hover:bg-red-400 hover:text-gray-900 transition-colors duration-300"
+                                    >
                                         View Repo
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
+                                    </Button>
+                                </Stack>
+                            </Stack>
+                        </Box>
                     ))}
-                </div>
+                </Group>
             ))}
-        </div>
+        </Stack>
     )
 }
 
