603dfb5feb883b08ea0cb55a376cdaa23cfb380f
keith <keithneo00@gmail.com>
2025-04-27 22:15:29 +0800

    refactor(contactForm, page): Converted to mantine components

--- app/components/contactForm.tsx
+++ app/components/contactForm.tsx
@@ -1,6 +1,7 @@
 // contact form component 
 'use client';
 
+import { Box, Button, Center, Group, Stack, Text, TextInput, Textarea } from "@mantine/core";
 import { useState, ChangeEvent, FormEvent } from "react";
 
 interface FormData {
@@ -53,89 +54,86 @@ const ContactForm = () => {
     };
 
     return (
-        <div className="w-[70%] bg-[#3d3131] rounded-3xl shadow-lg shadow-red-500 animate-appear opacity-0">
-            <div className="w-full pl-10 pt-6">
-                {/* <h3 className="text-gray-200 text-4xl font-heading mb-3">Drop Me a Message</h3> */}
-                <p className="w-11/12 font-sans text-gray-100 text-xl font-medium">
-                    I&apos;m excited to hear from you! Share your thoughts, ideas, or questions, and I&apos;ll get back to you soon.
-                </p>
-            </div>
-            <form 
-            onSubmit={handleSubmit}
-            className="w-auto h-auto p-5"
+        <Stack className="w-[70%] bg-[#3d3131] rounded-3xl shadow-lg shadow-red-500 animate-appear opacity-0">
+            <Box className="w-full pl-10 pt-6">
+                <Text className="w-11/12 font-sans text-gray-100 text-xl font-medium">I&apos;m excited to hear from you! Share your thoughts, ideas, or questions, and I&apos;ll get bck to you soon.</Text>
+            </Box>
+            <form
+                onSubmit={handleSubmit}
+                className="w-auto h-auto p-5"
             >
-                <div className="w-full flex justify-center mb-4 font-heading font-semibold">
-                    {successMessage && <p className="text-green-500 text-lg">{successMessage}</p>}
-                    {errorMessage && <p className="text-red-500 text-lg">{errorMessage}</p>}
-                </div>
-    
-            <div className="grid grid-cols-3 gap-y-5">
-                {/* name input */}
-                <label 
-                    htmlFor="name"
-                    className="text-gray-200 text-lg font-sans font-semibold ml-5"
-                >
-                    Name<span className="text-red-600">*</span>
-                </label>
-                <input 
-                    id="name" 
-                    type="text" 
-                    name="name"
-                    placeholder="Your Name"
-                    value={formData.name}
-                    onChange={handleChange}
-                    required
-                    className="col-span-2 rounded-md border px-2 py-1 focus:outline-none focus:border-blue-700 focus:ring-blue-700 focus:ring-1"
-                />
+                <Center className="w-full mb-4 font-heading font-semibold">
+                    {successMessage && <Text className="text-green-500 text-lg">{successMessage}</Text>}
+                    {errorMessage && <Text className="text-red-500 text-lg">{errorMessage}</Text>}
+                </Center>
 
-                {/* email input */}
-                <label 
-                    htmlFor="email"
-                    className="text-gray-200 text-lg font-sans font-semibold ml-5"
-                >
-                    Email<span className="text-red-600">*</span>
-                </label>
-                <input 
-                    id="email"
-                    type="email" 
-                    name="email"
-                    placeholder="Jane@example.com"
-                    value={formData.email}
-                    onChange={handleChange}
-                    required
-                    className="col-span-2 rounded-md border px-2 py-1 focus:outline-none focus:border-blue-700 focus:ring-blue-700 focus:ring-1"
-                />
+                <Stack>
+                    {/* name input */}
+                    <Group className="w-full" wrap="nowrap" gap={0}>
+                        <Group gap={1} className="w-1/3" align="center">
+                            <Text span className="text-gray-200 text-lg font-sans font-semibold ml-5">Name</Text>
+                            <Text span className="text-red-600">*</Text>
+                        </Group>
+                        <TextInput 
+                            id="name"
+                            name="name"
+                            placeholder="Your Name"
+                            value={formData.name}
+                            onChange={(e) => handleChange(e)}
+                            required
+                            className="w-2/3 px-2 py-1 focus:outline-none"
+                        />
+                    </Group>
 
-                {/* message input */}
-                <label 
-                    htmlFor="message"
-                    className="text-gray-200 text-lg font-sans font-semibold ml-5"
-                >
-                    Message<span className="text-red-600">*</span>
-                </label>
-                <textarea 
-                    name="message" 
-                    id="message"
-                    placeholder="Have a project idea or a question? Let me know!"
-                    value={formData.message}
-                    onChange={handleChange}
-                    required
-                    rows={5}
-                    className="col-span-2 rounded-md border px-2 py-1 focus:outline-none focus:border-blue-700 focus:ring-blue-700 focus:ring-1"
-                ></textarea>
-                <div className="col-span-3 flex justify-end">
-                    <button
-                        type="submit"
-                        disabled={isSubmitting}
-                        className="p-2 px-4 bg-red-400 text-base font-semibold rounded-md hover:bg-red-500 transition duration-200"
-                    >
-                        {isSubmitting ? 'Sending...' : 'Send Message'}
-                    </button>
-                </div>
-            </div>
-                
+                    {/* email input */}
+                    <Group className="w-full" wrap="nowrap" gap={0}>
+                        <Group gap={1} className="w-1/3" align="center">
+                            <Text span className="text-gray-200 text-lg font-sans font-semibold ml-5">Email</Text>
+                            <Text span className="text-red-600">*</Text>
+                        </Group>
+                        <TextInput 
+                            id="email"
+                            type="email"
+                            name="email"
+                            placeholder="Jane@example.com"
+                            value={formData.email}
+                            onChange={(e) => handleChange(e)}
+                            required
+                            className="w-2/3 px-2 py-1 focus:outline-none"
+                        />
+                    </Group>
+
+                    {/* message input */}
+                    <Group className="w-full" wrap="nowrap" gap={0} align="flex-start">
+                        <Group gap={1} className="w-1/3" align="center">
+                            <Text span className="text-gray-200 text-lg font-sans font-semibold ml-5">Message</Text>
+                            <Text span className="text-red-600">*</Text>
+                        </Group>
+                        <Textarea 
+                            id="message"
+                            name="message"
+                            placeholder="Have a project idea or a question? Let me know!"
+                            value={formData.message}
+                            onChange={(e) => handleChange(e)}
+                            required
+                            rows={5}
+                            className="w-2/3 px-2 py-1 focus:outline-none"
+                        />
+                    </Group>
+
+                    {/* submit button */}
+                    <Group className="w-full px-2" justify="flex-end">
+                        <Button
+                            type="submit"
+                            disabled={isSubmitting}
+                            className="py-2 px-4 bg-red-400 text-base font-semibold rounded-md hover:bg-red-500 transition duration-200"
+                        >
+                            {isSubmitting ? 'Sending...' : 'Send Message'}
+                        </Button>
+                    </Group>
+                </Stack>
             </form>
-        </div>
+        </Stack>
     )
 }
 
