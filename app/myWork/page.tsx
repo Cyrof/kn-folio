fbbaf554b6ca87ae2bc046784261551eb4503324
keith <keithneo00@gmail.com>
2025-04-28 01:41:52 +0800

    refactor(projects, skills, processed-data, myWork/page): Convert to mantine components Change the processed data json file to use the new path of the images
    The images will now be within the github repository itself instead of
    dynamically downloading it from google drive since it does not work too
    well.
    
    refactor(images/projects): Moved project images to its own folder
    Done this for organisation purposes

--- app/myWork/page.tsx
+++ app/myWork/page.tsx
@@ -4,46 +4,38 @@ import Footer from "../components/footer"
 import TextReveal from "../components/textReveal"
 import Skills from "../components/skills"
 import Projects from "../components/projects"
+import { Box, Center, Group, Stack, Text } from "@mantine/core"
 
 export default function myWork(){ 
     return(
-        <main>
+        <>
             <NavBar />
-            {/* main section wrapper */}
-            <section className="text-gray-200 w-full h-auto flex flex-col">
-                {/* header / banner */}
-                <section className="w-full">
-                    <div className="w-full h-auto flex justify-center items-center mt-5 ">
-                        <TextReveal />
-                    </div>
-                </section>
-                {/* top skills */}
-                <section className="my-16 h-5/6">
-                    <div className="w-full h-auto flex px-5">
-                        {/* header */}
-                        <div className="flex justify-center items-center h-auto w-[5%] m-0 opacity-0 animate-right-slideIn [animation-delay:500ms]">
-                            <h1 className="[writing-mode:vertical-lr] rotate-180 text-4xl text-gray-200">My Skills</h1>
-                        </div>
-                        <Skills />
-                        {/* placeholder */}
-                        <div className="w-[5%] h-auto"></div>
-                    </div>
-                </section>
-                <hr className="mx-10 border-[#2b1f1f]"/>
-                {/* projects here */}
-                <section className="my-16 h-4/6">
-                    <div className="w-full h-auto flex px-5">
-                        {/* placeholder */}
-                        <div className="w-[5%]"></div>
-                        <Projects />
-                        {/* header */}
-                        <div className="flex justify-center items-center h-auto w-[5%] animate-left-slidein opacity-0 [animation-timeline:view()] [animation-range-start:20%] [animation-range-end:80%]">
-                            <h1 className="[writing-mode:vertical-lr] text-4xl text-gray-200">My Projects</h1>
-                        </div>
-                    </div>
-                </section>
-            </section>
+            <Stack>
+                {/* header */}
+                <Center className="w-full h-auto mt-5">
+                    <TextReveal />
+                </Center>
+                {/* skills */}
+                <Group className="my-16 w-full h-5/6 px-5" gap={0} wrap="nowrap">
+                    <Center className="h-auto w-[5%] m-0 opacity-0 animate-right-slideIn [animation-delay:500ms]">
+                        <Text className="[writing-mode:vertical-lr] rotate-180 text-4xl text-gray-200">My Skills</Text>
+                    </Center>
+                    <Skills />
+                    {/* placeholder */}
+                    <Box className="w-[5%] h-auto" />
+                </Group>
+                {/* projects */}
+                <Group className="my-16 w-full h-5/6 px-5" gap={0} wrap="nowrap">
+                    {/* placeholder */}
+                    <Box className="w-[5%] h-auto"/>
+                    <Projects />
+                    {/* header */}
+                    <Center className="h-auto w-[5%] animate-left-slidein opacity-0 [animation-timeline:view()] [animation-range-start:10%] [animation-range-end:20%]">
+                        <Text className="[writing-mode:vertical-lr] text-4xl text-gray-200">My Projects</Text>
+                    </Center>
+                </Group>
+            </Stack>
             <Footer />
-        </main>
+        </>
     )
 }
