4ff8143fe0921e9fe5b5407817de92481faa6450
keith <keithneo00@gmail.com>
2025-04-27 22:15:29 +0800

    refactor(contactForm, page): Converted to mantine components

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -2,28 +2,24 @@
 import NavBar from "../components/navbar";
 import Footer from "../components/footer";
 import ContactForm from "../components/contactForm";
+import { Group, Stack, Text } from "@mantine/core";
 
 export default function Contact() {
     return (
-        <main>
+        <>
             <NavBar />
-            {/* main sectino */}
-            <section className="w-full h-auto flex flex-row my-16">
-                {/* text section left */}
-                <section className="w-1/2 h-auto">
-                    <div className="w-full flex justify-start mt-14 opacity-0 animate-staggered-text-down [--slidein-delay:200ms]">
-                        <h1 className="font-heading text-8xl text-gray-200 ml-28">Lets Get</h1>
-                    </div>
-                    <div className="w-full flex justify-end mt-10 opacity-0 animate-staggered-text-down [--slidein-delay:400ms]">
-                        <h1 className="font-heading text-8xl text-gray-200 mr-28">In Touch</h1>
-                    </div>
-                </section>
+            <Group className="w-full h-auto my-16" wrap="nowrap" gap={0}>
+                {/* left text section */}
+                <Stack className="w-1/2 h-auto">
+                    <Text className="font-heading text-8xl text-gray-200 ml-40 opacity-0 animate-staggered-text-down [--slidein-delay:200ms]">Lets Get</Text>
+                    <Text className="font-heading text-8xl text-gray-200 text-right mr-40 opacity-0 animate-staggered-text-down [--slidein-delay:400ms]">In Touch</Text>
+                </Stack>
                 {/* contact form right */}
-                <section className="w-1/2 h-auto flex">
+                <Group className="w-1/2 h-auto">
                     <ContactForm />
-                </section>
-            </section>               
+                </Group>
+            </Group>
             <Footer />
-        </main>
+        </>
     );
 }
